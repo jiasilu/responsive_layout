6cb36caf5d84aae26958009e017f3eadb10b78c4
Justin Jia <justin@goodinc.com>
2013-11-03 03:19:39 -0800

    Finish all 3 layouts with javascript.

---
+++ main.js
@@ -0,0 +1,19 @@
+(function() {
+  var dropdown = $(".dropdown");
+  $(".down_arrow_icon").on("click", function(event) {
+    event.preventDefault();
+    dropdown.toggle("slow");
+  });
+
+  $(".menu_icon").on("click", function(event) {
+    event.preventDefault();
+    if(dropdown.hasClass("top")) {
+      dropdown.toggle("slow");
+    } else {
+      dropdown.addClass("top").insertBefore("header");
+      dropdown.prepend($(".account .text"));
+      dropdown.hide();
+      dropdown.toggle("slow");
+    }
+  });
+})();
